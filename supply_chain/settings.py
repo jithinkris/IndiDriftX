6335bdd697df12894c8136e8e4aab5859cb5b1bc
JITHINKRISHNAN P G <115406608+jithinkris@users.noreply.github.com>
2026-06-11 10:50:42 +0530

    Update settings.py

--- supply_chain/settings.py
+++ supply_chain/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-supply-chain-shock-dev-key-change-in-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
